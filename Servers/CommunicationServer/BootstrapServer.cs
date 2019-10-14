c32408540fb7fc5d013b7f1fd09277beaba0ab70
ImBradders <100397313@00D8614724E2.university.ds.derby.ac.uk>
2019-10-14 15:32:04 +0100

    * rename main program for CommunicationServer\n+ add code for CommunicationServer boostrap with port number

---
+++ Servers/CommunicationServer/BootstrapServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CommunicationServer
+{
+    class BootstrapServer
+    {
+        static void Main(string[] args)
+        {
+            const int defaultPort = 57313;
+            int portToListenOn = defaultPort;
+
+            if (args.Length == 1)
+            {
+                if (!int.TryParse(args[0], out portToListenOn))
+                {
+                    portToListenOn = defaultPort;
+                }
+            }
+            
+            
+        }
+    }
+}
