f7c2201cc4f7931d940ad3d1c15b4a11adb7cc9e
ImBradders <bradley.davis@ntlworld.com>
2020-01-05 11:46:43 +0000

    Login + The login form to allow the user to log into the system
    SharedDataSource
    + Thread safe data source to allow for access to messages and responses.

---
+++ FrontEnd/Login.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System.ComponentModel;
+
+namespace FrontEnd
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblMessage.Location = new System.Drawing.Point(12, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(333, 81);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Welcome to Music Streaming Service!";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtUsername.Location = new System.Drawing.Point(113, 157);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(232, 29);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(12, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(12, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtPassword.Location = new System.Drawing.Point(113, 210);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(232, 29);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnLogin.Location = new System.Drawing.Point(198, 314);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(147, 41);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnCreateAccount.Location = new System.Drawing.Point(12, 314);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(147, 41);
+            this.btnCreateAccount.TabIndex = 6;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 367);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ FrontEnd/SharedDataSource.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FrontEnd
+{
+    public class SharedDataSource
+    {
+        private static SharedDataSource _instance = null;
+        private Queue<string> _messageQueue;
+        public event EventHandler AddedResponse;
+        private Queue<string> _responseQueue;
+
+        /// <summary>
+        /// Constructor for the singleton class to allow for the queues to be created.
+        /// </summary>
+        private SharedDataSource()
+        {
+            _messageQueue = new Queue<string>();
+            _responseQueue = new Queue<string>();
+        }
+
+        /// <summary>
+        /// Singleton accessor.
+        /// </summary>
+        /// <returns>The instance of the class.</returns>
+        public static SharedDataSource GetInstance()
+        {
+            return _instance ?? (_instance = new SharedDataSource());
+        }
+
+        /// <summary>
+        /// Adds a message to be sent to the server.
+        /// </summary>
+        /// <param name="message">The message to be added.</param>
+        public void AddMessage(string message)
+        {
+            lock (_messageQueue)
+            {
+                _messageQueue.Enqueue(message);
+            }
+        }
+
+        /// <summary>
+        /// Get a message from the list of messages to be sent.
+        /// </summary>
+        /// <returns>A message to be sent to the server.</returns>
+        public string GetMessage()
+        {
+            string message;
+            lock (_messageQueue)
+            {
+                message = _messageQueue.Dequeue();
+            }
+            
+            return message;
+        }
+
+        /// <summary>
+        /// Fires off an event handler, if one is subscribed, to process a message when it is added.
+        /// </summary>
+        private void OnAddResponse()
+        {
+            AddedResponse?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Adds a response to the response queue and activates the response that says that this has been added.
+        /// </summary>
+        /// <param name="response">The response from the server.</param>
+        public void AddResponse(string response)
+        {
+            lock (_responseQueue)
+            {
+                _responseQueue.Enqueue(response);
+            }
+            OnAddResponse();
+        }
+
+        /// <summary>
+        /// Gets a response from the response queue.
+        /// </summary>
+        /// <returns>A response from the server.</returns>
+        public string GetResponse()
+        {
+            string response;
+            lock (_responseQueue)
+            {
+                response = _responseQueue.Dequeue();
+            }
+
+            return response;
+        }
+    }
+}
